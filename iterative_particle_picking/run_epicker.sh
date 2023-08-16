a768edb5d9aa18ef887c234f0e8958a0a66accc8
Tony Li <55900779+orange472@users.noreply.github.com>
2023-08-16 13:12:21 -0500

    Convert run_epicker.sh to bash script

--- iterative_particle_picking/run_epicker.sh
+++ iterative_particle_picking/run_epicker.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #	track runtime
 START=$(date +%s.%N)
 
@@ -6,18 +8,18 @@ if [ -z "${REPIC_MRC_DIR}" ]; then REPIC_MRC_DIR=0; fi
 if [ -z "${REPIC_BOX_SIZE}" ]; then REPIC_BOX_SIZE=0; fi
 if [ -z "${REPIC_OUT_DIR}" ]; then REPIC_OUT_DIR=0; fi
 if [ -z "${REPIC_UTILS}" ]; then REPIC_UTILS=0; fi
-if [ -z "${EPICKER_ENV}" ]; then CRYOLO_ENV="epicker"; fi
-if [ -z "${EPICKER_DIR}" ]; then DEEP_DIR="./EPicker"; fi
-if [ -z "${EPICKER_MODEL}" ]; then EPICKER_MODEL=${EPICKER_DIR}/Models/20210416; fi
+if [ -z "${EPICKER_ENV}" ]; then EPICKER_ENV="epicker"; fi
+if [ -z "${EPICKER_DIR}" ]; then EPICKER_DIR="./EPicker"; fi
+if [ -z "${EPICKER_MODEL}" ]; then :; fi
 
 eval "$(conda shell.bash hook)"
-conda activate ${CRYOLO_ENV}
+conda activate ${EPICKER_ENV}
 
 # Run EPicker picking script
-${EPICKER_DIR}/EPicker/bin/epicker.sh \
+${EPICKER_DIR}/bin/epicker.sh \
   --data=${REPIC_MRC_DIR} \
-  --load_model=${EPICKER_MODEL}/model_best.pth \
-  --output=${REPIC_OUT_DIR} \
+  --load_model=${EPICKER_MODEL} \
+  --output=${REPIC_OUT_DIR}/output \
   --output_type="box"
 
 conda deactivate
@@ -28,4 +30,4 @@ DIFF=$(echo "${END} - ${START}" | bc -l)
 LABEL=$(basename ${REPIC_MRC_DIR} | sed -E 's/_[0-9]+//g')
 COUNT=$(ls ${REPIC_MRC_DIR}/*.mrc | wc -l)
 echo -e """start\tend\tdifference\tN
-${START}\t${END}\t${DIFF}\t${COUNT}""" >${REPIC_OUT_DIR}/run_cryolo_runtime_${LABEL}.tsv
+${START}\t${END}\t${DIFF}\t${COUNT}""" >${REPIC_OUT_DIR}/run_epicker_runtime_${LABEL}.tsv
